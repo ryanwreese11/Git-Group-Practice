7af7bebc0d9f88bf27a9e0c1740bf66ccb49c87d
Ryan <ryan.reese11@gmail.com>
2019-05-09 16:46:13 -0600

    merge2

--- src/App.js
+++ src/App.js
@@ -43,8 +43,8 @@ class App extends Component {
               <h5>Add your name below</h5>
               <h4>Signed in:</h4>
               <ul>
-                <li>Ryan Reese</li>
                 
+                <li>Ryan Reese</li>
               </ul>
             </div>
           </div>
