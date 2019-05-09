bd596ebf08a6c5e7592181610ee41093cd193451
Tanner Scadden <tanner@scaddenfamily.com>
2019-05-09 16:33:30 -0600

    pushed

--- src/App.js
+++ src/App.js
@@ -44,8 +44,7 @@ class App extends Component {
               <h5>Add your name below</h5>
               <h4>Signed in:</h4>
               <ul>
-                <li>Spencer Smith</li>
-                {/* add name here*/}
+                <li>Tanner Scadden</li>
               </ul>
             </div>
           </div>
